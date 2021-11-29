47112547f76c006a53f51871ffdff7b216c8e343
Felipe da Silva Martins <femartins2010@gmail.com>
2021-11-29 00:43:27 -0300

    system files

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const ul = document.querySelector('.tasks')
 const input = document.querySelector('input')
-let lis = document.querySelectorAll('li')
+const tasks = []
 const del = document.querySelector('.delete')
+const addButton = document.querySelector('button')
 
 function addTask() {
     let task = input.value.trim()
+
     if (task == '' || task == null)
         alert('Adicione  uma tarefa')
     else {
-        let li = document.createElement('li')
+        tasks.push(task)
+        li = document.createElement('li')
         li.innerHTML = task
         ul.appendChild(li)
+        saveStorage()
         input.value = ''
     }
 }
 
 function markTask() {
-
-    lis.forEach((e, i) => {
-        e.onclick = event => {
+    let lis = document.querySelectorAll('.tasks li')
+    lis.forEach((e) => {
+        e.onclick = () => {
             let elementStyle = e.style
             if (elementStyle.textDecoration === 'line-through') {
                 elementStyle.textDecoration = 'none'
@@ -31,27 +35,51 @@ function markTask() {
 }
 
 function removeTask() {
+    let lis = document.querySelectorAll('.tasks li')
+
     lis.forEach(e => {
-        e.onclick = event => {
+        e.onclick = () => {
             ul.removeChild(e)
+            tasks.pop(e)
+            saveStorage()
         }
     })
     lis = document.querySelectorAll('li')
 }
 
+function saveStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
 
-document.querySelector('button').onclick = () => {
+addButton.onclick = () => {
     addTask()
     lis = document.querySelectorAll('li')
     markTask()
     del.classList.remove('off')
     del.classList.add('on')
+}
+document.addEventListener("keypress", (e) => {
+    if (e.key === "Enter")
+        addTask()
+})
 
+window.onload = () => {
+    const storageTasks = localStorage.getItem('tasks')
+
+    if (storageTasks) {
+        tasks.push(...JSON.parse(storageTasks))
+        tasks.map(task => {
+            const li = document.createElement('li')
+            li.innerHTML = task
+            ul.appendChild(li)
+        })
+    }
+    markTask()
 }
 
 del.onclick = () => {
-    let on = del.classList
-    if (on.contains('on')) {
+    let classes = del.classList
+    if (classes.contains('on')) {
         del.classList.remove('on')
         del.classList.add('off')
         removeTask()
@@ -62,7 +90,4 @@ del.onclick = () => {
     }
 }
 
-window.onload = () => {
-    markTask()
-}
 
